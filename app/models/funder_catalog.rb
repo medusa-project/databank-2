5e8fd41bebe86c815f30eb9e7d7633555300d9e8
mfall3 <mfall3@illinois.edu>
2026-08-17 15:02:00 -0500

    undo funder name abbreviation

--- app/models/funder_catalog.rb
+++ app/models/funder_catalog.rb
@@ -4,13 +4,13 @@ class FunderCatalog
   OTHER_CODE = "other".freeze
 
   ENTRIES = [
-    Entry.new(code: "IDCEO", name: "Illinois Dept. of Commerce & Economic Opportunity (DCEO)", identifier: "10.13039/100004885", display_position: 10, identifier_scheme: "DOI"),
-    Entry.new(code: "IDHS", name: "Illinois Dept. of Human Services (DHS)", identifier: "10.13039/100004886", display_position: 20, identifier_scheme: "DOI"),
-    Entry.new(code: "IDNR", name: "Illinois Dept. of Natural Resources (IDNR)", identifier: "10.13039/100004887", display_position: 30, identifier_scheme: "DOI"),
-    Entry.new(code: "IDOT", name: "Illinois Dept. of Transportation (IDOT)", identifier: "10.13039/100009637", display_position: 40, identifier_scheme: "DOI"),
+    Entry.new(code: "IDCEO", name: "Illinois Deptartment of Commerce & Economic Opportunity (DCEO)", identifier: "10.13039/100004885", display_position: 10, identifier_scheme: "DOI"),
+    Entry.new(code: "IDHS", name: "Illinois Deptartment of Human Services (DHS)", identifier: "10.13039/100004886", display_position: 20, identifier_scheme: "DOI"),
+    Entry.new(code: "IDNR", name: "Illinois Department of Natural Resources (IDNR)", identifier: "10.13039/100004887", display_position: 30, identifier_scheme: "DOI"),
+    Entry.new(code: "IDOT", name: "Illinois Deptartment of Transportation (IDOT)", identifier: "10.13039/100009637", display_position: 40, identifier_scheme: "DOI"),
     Entry.new(code: "USARMY", name: "U.S. Army", identifier: "10.13039/100006751", display_position: 50, identifier_scheme: "DOI"),
-    Entry.new(code: "USDA", name: "U.S. Dept. of Agriculture (USDA)", identifier: "10.13039/100000199", display_position: 60, identifier_scheme: "DOI"),
-    Entry.new(code: "DOE", name: "U.S. Dept. of Energy (DOE)", identifier: "10.13039/100000015", display_position: 70, identifier_scheme: "DOI"),
+    Entry.new(code: "USDA", name: "U.S. Deptartment of Agriculture (USDA)", identifier: "10.13039/100000199", display_position: 60, identifier_scheme: "DOI"),
+    Entry.new(code: "DOE", name: "U.S. Deptartment of Energy (DOE)", identifier: "10.13039/100000015", display_position: 70, identifier_scheme: "DOI"),
     Entry.new(code: "USGS", name: "U.S. Geological Survey (USGS)", identifier: "10.13039/100000203", display_position: 80, identifier_scheme: "DOI"),
     Entry.new(code: "NASA", name: "U.S. National Aeronautics & Space Administration (NASA)", identifier: "10.13039/100000104", display_position: 90, identifier_scheme: "DOI"),
     Entry.new(code: "NIH", name: "U.S. National Institutes of Health (NIH)", identifier: "10.13039/100000002", display_position: 100, identifier_scheme: "DOI"),
